570f64b4d0652b2b96c9717dbb4047629ab5b506
skrevolve <sukyu0919@naver.com>
2023-05-03 18:10:13 +0900

    init

--- wss/client/main.js
+++ wss/client/main.js
@@ -1,15 +1,44 @@
 const WebSocket = require("ws")
+const readline = require("readline")
+
 const socket = new WebSocket("ws://localhost:3000/ws/123?v=1.0")
 
-socket.onopen = function (e) {
-  socket.send("hello")
-}
+let clientMessage
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
 
-socket.onmessage = async function (e) {
+rl.on("line", (line) => {
 
-}
+  console.log("input: ", line)
+  clientMessage = line
 
-socket.onerror = function(e) {
-  console.log(e)
-}
+  socket.onopen = (e) => {
+
+    socket.send(clientMessage)
+
+    socket.onmessage = (e) => {
+      console.debug("client received a message")
+      // console.debug(e)
+    }
+
+    socket.onclose = (e) => {
+      console.debug("client notified socket has closed")
+      // console.debug(e)
+    }
+
+    socket.onerror = (e) => {
+      console.debug(e.message) // console.log(e)
+    }
+
+    // socket.close()
+  }
+
+  // rl.close()
+})
+
+rl.on("close", () => {
+  process.exit()
+})
